faa837fac82691764d9873c36133b2dca6fb1e34
Jess <jessica_yara@yahoo.com.br>
2021-06-28 18:22:27 -0300

    Cadastro autor

---
+++ casaDoCodigo/src/main/java/com/orangeTalents/casaDoCodigo/Autor/AutorDto.java
@@ -0,0 +1,62 @@
+package com.orangeTalents.casaDoCodigo.Autor;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+
+public class AutorDto {
+	@NotBlank
+	private String nome;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	@Size(max=400)
+	private String descricao;
+	@NotNull
+	private LocalDateTime dataCriacao= LocalDateTime.now();
+
+	public AutorDto(@NotBlank String nome, @NotBlank @Email String email, @NotBlank @Size(max=400) String descricao) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.descricao = descricao;
+		
+	}
+
+	
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	public  Autor toModel() {
+		return new Autor(this.nome, this.email, this.descricao);
+	}
+
+
+	
+	
+}
